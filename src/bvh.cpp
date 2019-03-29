68bd5e066625b81118f1e9eef5267cf22c8e4c79
Zhugucai <gaps_zgc@zju.edu.cn>
2019-03-29 20:11:01 +0800

    add bvh file

---
+++ src/bvh.cpp
@@ -0,0 +1,172 @@
+#include "bvh.h"
+
+void BVH::build_bvh(const vector<Triangle*>& triangles)
+{
+	root = construct_bvh(triangles);
+}
+
+//helper func to compute the centroid in which bucket
+int compute_bucket(double centroid, double min_val, double max_val, int bucket_num)
+{
+	double idx;
+	idx = ((centroid - min_val) / (max_val - min_val)) * bucket_num;
+	return (int)idx;
+}
+
+//compare func, use centroid to sort the triangle
+bool triangleCompare(const Triangle *p1, const Triangle *p2, int split_axis)
+{
+	double v1, v2;
+	v1 = p1->get_bbox().centroid()[split_axis];
+	v2 = p2->get_bbox().centroid()[split_axis];
+	return v1 < v2;
+}
+
+BVHNode* BVH::construct_bvh(const std::vector<Triangle*>& triangles) {
+
+#define max_leaf_size 4
+
+	BBox centroid_box, bbox;
+	bool use_sah = false;
+
+	//compute the bbox tris
+	for (Triangle *p : triangles) {
+		BBox bb = p->get_bbox();
+		bbox.expand(bb);
+		Vector3D c = bb.centroid();
+		centroid_box.expand(c);
+	}
+
+	BVHNode *node = new BVHNode(bbox);
+
+	if (triangles.size() <= max_leaf_size)
+	{
+		node->triangles = new vector<Triangle *>(triangles);
+		return node;
+	}
+	else //bvh use sah
+	{
+		int split_axis = 0;
+		double max_extent = centroid_box.extent[0];
+		for (int i = 1; i < 3; i++)
+		{
+			if (max_extent < centroid_box.extent[i])
+			{
+				max_extent = centroid_box.extent[i];
+				split_axis = i;
+			}
+		}
+		if (use_sah) {
+#define Bucket_num 64
+			BBoxBucket bboxBucket[Bucket_num];
+			BBoxBucket bboxBucketPreSum[Bucket_num];
+			BBoxBucket bboxBucketRevSum[Bucket_num];
+			double min_val = bbox.min[split_axis];
+			double max_val = bbox.max[split_axis];
+			for (Triangle *p : triangles)
+			{
+				int idx = compute_bucket(p->get_bbox().centroid()[split_axis], min_val, max_val, Bucket_num);
+				//cout << endl << "cal idx : " << idx << endl;
+				bboxBucket[idx].expand(p);
+			}
+			bboxBucketPreSum[0].expand(bboxBucket[0]);
+			bboxBucketRevSum[Bucket_num - 1].expand(bboxBucket[Bucket_num - 1]);
+			for (int i = 1; i < Bucket_num; i++)
+			{
+				bboxBucketPreSum[i].expand(bboxBucket[i]);
+				bboxBucketPreSum[i].expand(bboxBucketPreSum[i - 1]);
+				//cout << "Pre Size " << i << " : " << bboxBucketPreSum[i].p_list.size() << endl;
+				bboxBucketRevSum[Bucket_num - i - 1].expand(bboxBucket[Bucket_num - i - 1]);
+				bboxBucketRevSum[Bucket_num - i - 1].expand(bboxBucketRevSum[Bucket_num - i]);
+				//cout << "Rev Size " << bucket_num - i - 1 << " : " << bboxBucketRevSum[bucket_num - i - 1].p_list.size() << endl;
+			}
+			double max_cost = (double)triangles.size() * bbox.surface_area();
+			int partition_num = Bucket_num - 1;
+			for (int i = 0; i < Bucket_num - 1; i++)
+			{
+				double this_cost = bboxBucketPreSum[i].get_cost() + bboxBucketRevSum[i + 1].get_cost();
+				if (this_cost < max_cost)
+				{
+					partition_num = i;
+					max_cost = this_cost;
+				}
+			}
+			if (partition_num == Bucket_num - 1)//partition doesnt help
+			{
+				//node->prims = new vector<Primitive *>(prims);
+				//return node;
+				size_t mid_element_idx = triangles.size() / 2;
+				auto cp_prims = vector<Triangle *>(triangles);
+				auto compare_func = [split_axis](Triangle *p1, Triangle *p2) {return triangleCompare(p1, p2, split_axis); };
+				std::nth_element(cp_prims.begin(), cp_prims.begin() + mid_element_idx, cp_prims.end(), compare_func);
+				vector<Triangle *> left = vector<Triangle *>(cp_prims.begin(), cp_prims.begin() + mid_element_idx);
+				vector<Triangle *> right = vector<Triangle *>(cp_prims.begin() + mid_element_idx, cp_prims.end());
+				node->l = construct_bvh(left);
+				node->r = construct_bvh(right);
+				return node;
+			}
+			else
+			{
+				//cout << "left size : " << bboxBucketPreSum[partition_num].p_list.size() << endl;
+				//cout << "right size : " << bboxBucketRevSum[partition_num + 1].p_list.size() << endl;
+				node->l = construct_bvh(bboxBucketPreSum[partition_num].p_list);
+				node->r = construct_bvh(bboxBucketRevSum[partition_num + 1].p_list);
+				return node;
+			}
+		}
+		else// use mid element partition
+		{
+			size_t mid_element_idx = triangles.size() / 2;
+			auto cp_prims = vector<Triangle *>(triangles);
+			auto compare_func = [split_axis](Triangle *p1, Triangle *p2) {return triangleCompare(p1, p2, split_axis); };
+			std::nth_element(cp_prims.begin(), cp_prims.begin() + mid_element_idx, cp_prims.end(), compare_func);
+			vector<Triangle *> left = vector<Triangle *>(cp_prims.begin(), cp_prims.begin() + mid_element_idx);
+			vector<Triangle *> right = vector<Triangle *>(cp_prims.begin() + mid_element_idx, cp_prims.end());
+			node->l = construct_bvh(left);
+			node->r = construct_bvh(right);
+			return node;
+		}
+	}
+	return node;
+}
+
+bool BVH::intersect(Ray& ray, BVHNode *node) const {
+
+	bool hit = false;
+	double min_t = ray.min_t;
+	double max_t = ray.max_t;
+	if (!node->bb.intersect(ray, min_t, max_t))
+		return hit;
+	if (node->isLeaf())
+	{
+		for (Triangle *p : *(node->triangles)) {
+			if (p->intersect(ray))
+				return true;
+		}
+		return hit;
+	}
+	hit |= intersect(ray, node->l);
+	if (hit) return hit;
+	hit |= intersect(ray, node->r);
+	return hit;
+}
+
+bool BVH::intersect(Ray& ray, Intersection* i, BVHNode *node) const 
+{
+	bool hit = false;
+	double min_t = ray.min_t;
+	double max_t = ray.max_t;
+	if (!node->bb.intersect(ray, min_t, max_t))
+		return hit;
+	if (node->isLeaf())
+	{
+		for (Triangle *p : *(node->triangles)) {
+			if (p->intersect(ray, i))
+				hit = true;
+		}
+		return hit;
+	}
+	hit |= intersect(ray, i, node->l);
+	hit |= intersect(ray, i, node->r);
+	return hit;
+}
---
+++ src/bvh.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "BBox.h"
+#include "scene/triangle.h"
+#include "intersection.h"
+
+struct BVHNode
+{
+	BVHNode(BBox _bb) : l(NULL), r(NULL), bb(_bb), triangles(NULL) {};
+	~BVHNode() { if (triangles) delete triangles; if (l) delete l; if (r) delete r; }
+	BVHNode* l;
+	BVHNode* r;
+	vector<Triangle*> *triangles;
+	BBox bb;
+
+	inline bool isLeaf() const { return l == NULL && r == NULL; }
+};
+
+class BVH
+{
+public:
+	BVH() {};
+	~BVH() { if (root) delete root; };
+
+	void build_bvh(const vector<Triangle*> &triangles);
+
+	//when meet intersect return imediately
+	bool intersect(Ray& r) const {
+		return intersect(r, root);
+	}
+
+	//meet all possible intersect
+	bool intersect(Ray& r, Intersection* i) const {
+		return intersect(r, i, root);
+	}
+private:
+	BVHNode *construct_bvh(const std::vector<Triangle*>& prims);
+	bool intersect(Ray& r, BVHNode* node) const;
+	bool intersect(Ray& r, Intersection* i, BVHNode* node) const;
+	BVHNode* root;
+	
+};
+
+
+struct BBoxBucket {
+	BBox bbox;
+	int prim_count;
+	std::vector<Triangle *> p_list;
+	BBoxBucket() : bbox(), p_list() { prim_count = 0; p_list.clear(); }
+	void expand(BBoxBucket &bbb)
+	{
+		bbox.expand(bbb.bbox);
+		p_list.insert(p_list.end(), bbb.p_list.begin(), bbb.p_list.end());
+		prim_count += bbb.prim_count;
+	}
+	void expand(Triangle *p)
+	{
+		bbox.expand(p->get_bbox());
+		p_list.push_back(p);
+		prim_count++;
+	}
+	double get_cost()
+	{
+		return bbox.surface_area() * (double)p_list.size();
+	}
+};
